b36bdc20df4032e4f7f12dc94ef81a9e82e503aa
Roma <rkekua9@gmail.com>
2024-06-11 11:48:35 +0400

    Create Script

---
+++ sqlMonitoring.ps1
@@ -0,0 +1,26 @@
+﻿$physicalRAMMemory=(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
+
+while($true)
+{
+    Write-Host "Monitoring ....."
+
+    $sqlMemoryOcupied = get-process -Name sqlservr | 
+        select name, @{Name = "Private Working Set"; Expression = { 
+                $ProcessID = $_.ID; [math]::Round((gwmi Win32_PerfFormattedData_PerfProc_Process | 
+                        ? {$_.IDprocess -eq $ProcessID }).WorkingSetPrivate / 1Gb, 0)}}
+
+    if($sqlMemoryOcupied.'Private Working Se' -ge 1)
+    {
+        if($sqlMemoryOcupied.'Private Working Se' / $physicalRAMMemory.Value * 100 -ge 80)
+        {
+            $temp=get-process -Name sqlservr
+            Write-Host "Start Killing Server"
+            $temp.Kill()
+            $temp.WaitForExit();
+            Write-Host "Start Server"
+            net start MSSQLSERVER
+        }
+    }
+    Start-Sleep -s 15
+}
+
